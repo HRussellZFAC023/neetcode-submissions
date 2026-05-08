00534061aa6b5b577dc724983f93617549be8d64
Henry Russell <henry@therussellhouse.com>
2026-05-08 19:51:47 +0100

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number) {
+
+        for(let i = 0; i < nums.length; i++){
+            const numToFind = target - nums[i];
+
+            const j = nums.indexOf(numToFind, i + 1);
+            if(j !== -1){
+                return [i, j]
+            }
+
+        }
+
+    }
+}
